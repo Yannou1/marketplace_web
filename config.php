f3993d408e0467a5e0866ad1ed002722326fcafb
Yanis GERARD <116731477+Yannou1@users.noreply.github.com>
2023-06-26 19:33:51 +0100

    simple auth method

--- config.php
+++ config.php
@@ -10,24 +10,20 @@ if ($conn->connect_error) {
     die("Connexion échouée : " . $conn->connect_error);
 }
 
-// Le reste de votre code pour interagir avec la base de données et afficher les données dans la page web
+
 $nom = $_POST['nom'];
 $email = $_POST['email'];
+$password = $_POST['passwords'];
 
-// Effectuer les opérations sur la base de données
-// ...
-
-// Exemple : Insérer les données dans une table
-$sql = "INSERT INTO user (username, email) VALUES ('$nom', '$email')";
+$sql = "INSERT INTO user (username, email, password) VALUES ('$nom', '$email', '$password')";
 
 if ($conn->query($sql) === true) {
-    echo "Enregistrement réussi";
-} else {
-    echo "Erreur lors de l'enregistrement : " . $conn->error;
+    echo "<script>alert('Enregistrement réussi'); window.location.href = 'account.html';</script>";
+    exit();
+}
+ else {
+    echo "<script>alert('Erreur lors de l\'enregistrement : " . $conn->error . "');</script>";
 }
 
-// Fermer la connexion à la base de données
-$conn->close();
-?>
 $conn->close();
 ?>
